8d6d4021ea09817b905c193aeb47f061694e613b
haixiao2016 <haixiao@yeah.net>
2020-08-13 12:01:27 +0800

    fea: dad

--- server/router/index.js
+++ server/router/index.js
@@ -63,16 +63,18 @@ route.get("/facebook/userInfo", async(ctx, next) => {
   try {
     // ? why https://www.haixiao.online need `/`
     const response = await axios.get("https://graph.facebook.com/v8.0/oauth/access_token", {
-      // client_id: "320868815625915",
-      redirect_uri: "https://www.haixiao.online/",
-      client_secret: "d3cbe95cdb62eed889d52f706da07862",
-      code: ctx.query.code
+      params: {
+        client_id: "320868815625915",
+        redirect_uri: "https://www.haixiao.online/",
+        client_secret: "d3cbe95cdb62eed889d52f706da07862",
+        code: ctx.query.code
+      }
     })
     ctx.body = response.data
     logger.log(response.data)
     console.log(response)
   } catch (error) {
-    logger.error(error.response.data)
+    logger.error(error)
     ctx.status = 500
     ctx.body = error.response.data
   }
